a4546b4e72007fd3a60d04ae03293587da75e4ce
tyagi-achint <achinttyagi001@gmail.com>
2023-09-22 15:24:45 +0530

    image

--- partials/_footer.php
+++ partials/_footer.php
@@ -1,5 +1,5 @@
 <footer>
-    Copyright © 2023. All rights reserved.
+    Copyright © <?php echo date('Y');?>. All rights reserved.
 
     <p>
         Designed &amp; Developed by <a target="_blank" href="https://tyagi-achint.github.io/">Achint Tyagi</a>
