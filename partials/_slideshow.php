404d09df7a6ba27dd2d1c17261eb59f61654c959
tyagi-achint <achinttyagi001@gmail.com>
2023-09-23 13:55:24 +0530

    slideshow

--- partials/_slideshow.php
+++ partials/_slideshow.php
@@ -1,13 +1,25 @@
 <div class="slideshow-container">
     <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
     <div class="slide fade">
-        <img src="https://source.unsplash.com/1700x600/?java" alt="Image 1">
+        <img src="images/slide-1.jpeg" alt="Image 1">
     </div>
     <div class="slide fade">
-        <img src="https://source.unsplash.com/1700x600/?books" alt="Image 2">
+        <img src="images/slide-2.jpeg" alt="Image 2">
     </div>
     <div class="slide fade">
-        <img src="https://source.unsplash.com/1700x600/?css" alt="Image 3">
+        <img src="images/slide-3.jpeg" alt="Image 3">
+    </div>
+    <div class="slide fade">
+        <img src="images/slide-4.jpeg" alt="Image 4">
+    </div>
+    <div class="slide fade">
+        <img src="images/slide-5.jpeg" alt="Image 5">
+    </div>
+    <div class="slide fade">
+        <img src="images/slide-6.jpeg" alt="Image 6">
+    </div>
+    <div class="slide fade">
+        <img src="images/slide-7.jpeg" alt="Image 7">
     </div>
 
     <a class="next" onclick="plusSlides(1)">&#10095;</a>
@@ -15,5 +27,10 @@
         <span class="dot" onclick="currentSlide(1)"></span>
         <span class="dot" onclick="currentSlide(2)"></span>
         <span class="dot" onclick="currentSlide(3)"></span>
+        <span class="dot" onclick="currentSlide(4)"></span>
+        <span class="dot" onclick="currentSlide(5)"></span>
+        <span class="dot" onclick="currentSlide(6)"></span>
+        <span class="dot" onclick="currentSlide(7)"></span>
+
     </div>
 </div>
